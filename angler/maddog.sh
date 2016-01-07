2260aa0e6ae5b302ca33956294dc0f304c82d09d
Alcolawl <tdean@live.esu.edu>
2016-01-07 15:14:20 -0500

    Add MadDog Script for the Nexus 6P
    Settings discovered / created by Attaint. Brings MadDog Interactive
    Governor Tweaks to the Nexus 6P.

---
+++ angler/maddog.sh
@@ -0,0 +1,59 @@
+#Script created by Alcolawl - 1/07/2016 - Please give credit when using this in your work!
+echo ----------------------------------------------------
+echo Applying 'MadDog' Interactive Governor Settings
+echo ----------------------------------------------------
+
+#Apply settings to LITTLE cluster
+echo Applying settings to LITTLE cluster
+#Temporarily change permissions to governor files for the LITTLE cluster to enable Interactive governor
+chmod 644 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+chmod 444 /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+#Tweak Interactive Governor
+echo 82 600000:80 768000:80 960000:69 1248000:84 1478000:85 1550000:90 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
+echo -1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_slack
+echo 672000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
+echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
+echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
+echo 82 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
+echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
+echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
+echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/boostpulse_duration
+
+#Apply settings to Big cluster
+echo Applying settings to Big cluster
+#Temporarily change permissions to governor files for the Big cluster to enable Interactive governor
+echo 1 > /sys/devices/system/cpu/cpu4/online								#Online Core 4
+chmod 644 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+echo interactive > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+chmod 444 /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+#Temporarily change permissions to governor files for the Big cluster to lower minimum frequency to 384MHz
+chmod 644 /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+echo 384000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq			#Core 4 Minimum Frequency = 384MHz
+chmod 444 /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+#Tweak Interactive Governor
+echo 82 633000:74 768000:80 960000:69 1248000:83 1536000:84 1632000:85 1824000:90 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
+echo -1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_slack
+echo 633000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
+echo 10000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
+echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
+echo 72 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
+echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
+echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
+echo 0 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/boostpulse_duration
+
+#Disable Input Boost
+echo Disabling Input Boost
+echo 0:0 1:0 2:0 3:0 4:0 5:0 6:0 7:0 > /sys/module/cpu_boost/parameters/input_boost_freq
+echo 0 > /sys/module/cpu_boost/parameters/boost_ms
+echo 0 > /sys/module/cpu_boost/parameters/input_boost_ms
+#Disable TouchBoost
+echo Disabling TouchBoost
+echo 0 > /sys/module/msm_performance/parameters/touchboost
+#Disable Core Control and Enable Thermal Throttling allowing for longer sustained performance
+echo Disabling Aggressive CPU Thermal Throttling
+echo 0 > /sys/module/msm_thermal/core_control/enabled
+echo Y > /sys/module/msm_thermal/parameters/enabled
+echo ----------------------------------------------------
+echo Settings Successfully Applied! You may now tweak them further in ElementalX Kernel Manager
+echo ----------------------------------------------------
